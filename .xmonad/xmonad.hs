763626a7ae2f1a5aa65bb0fc93e4e71f5fc10c40
dulanov <dulanov@gmail.com>
2009-12-29 19:05:21 +0300

    initial setting for xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,25 @@
 import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Hooks.DynamicLog
 
-main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
+myKeys = [
+        ("M-<Right>",       windows W.focusDown)
+        , ("M-S-<Right>",   windows W.swapDown)
+        , ("M-<Left>",      windows W.focusUp)
+        , ("M-S-<Left>",     windows W.swapUp)
+    ]
+
+myLogHook = dynamicLogWithPP xmobarPP
+
+myManageHook = composeAll [
+        className =? "Firefox-bin" --> moveTo "web"    
+    ]
+        where moveTo = doF . W.shift
+
+main = xmonad defaultConfig {
+        modMask = mod4Mask -- Use Super instead of Alt
         , terminal = "urxvt"
-        }
+        , keys = myKeys
+        , logHook = myLogHook
+        , manageHook = myManageHook
+    }
