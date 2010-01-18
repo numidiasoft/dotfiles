0c9497f8fe86b564ce49ee53a913a36120556c9f
dulanov <dulanov@gmail.com>
2010-01-18 17:14:47 +0300

    xmonad startup settings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,23 @@
 import XMonad
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.DynamicLog
+import XMonad.Util.EZConfig (additionalKeys)
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
-import XMonad.Hooks.DynamicLog
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.LayoutHint (layoutHint)
 
 myBorderWidth = 2
 myNormalBorderColor = "#202030"
 myFocusedBorderColor = "#A0A0D0"
 
-myWorkspaces = ["web", "work", "virt"]
+myWorkspaces = ["web", "work", "virt", "4", "5", "6", "7"]
+
+--myLayout = smartBorders
+--        $ onWorkspace "web" (browseLayout ||| Full)
+--        $ genericLayout
+--    where
+--        browseLayout = Mirror Tall (3 % 100) (3 % 5)
 
 myBar = "xmobar"
 
@@ -23,6 +32,7 @@ myConfig = defaultConfig {
         , borderWidth = myBorderWidth
         , normalBorderColor = myNormalBorderColor
         , focusedBorderColor = myFocusedBorderColor
+--        , layoutHook = myLayout
     }
 
 main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
--- .zshrc
+++ .zshrc
@@ -1,13 +1,12 @@
 export PATH=/opt/local/bin:/opt/local/sbin:$PATH
 export LANG=en_US.UTF-8
+export TERM=rxvt-unicode
 export EDITOR=vi
 
 PROMPT=$'%{\e[1;32m%}> %{\e[0m%}'
 RPROMPT=$'%{\e[1;34m%}%~ %{\e[1;32m%}%#%{\e[0m%}'
 
-alias la="ls -a"
-alias lf="ls -F"
-alias ll="ls -l"
+alias ls="ls --color=auto"
 
 alias du="du -h"
 alias df="df -h"
